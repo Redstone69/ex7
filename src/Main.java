2e464663cf1a06b60955c32ac686d3fa5f23c7ab
Redstone69 <89121962+Redstone69@users.noreply.github.com>
2025-02-27 13:47:45 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hellword");
         Person hazem = new Person("haezm", "ahmed");
-        hazem.display();
+        buizz_contact buizzContact2 = new buizz_contact("haezm", "ahmed","1000");
+        buizzContact2.display();
+
+
 
 
     }}
