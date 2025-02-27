31d48fe9bfce80d3a2cb2229fe1edcd6efeccbb0
Redstone69 <89121962+Redstone69@users.noreply.github.com>
2025-02-27 13:47:08 -0500

    Update buizz_contact.java

--- src/buizz_contact.java
+++ src/buizz_contact.java
@@ -1,3 +1,30 @@
 public class buizz_contact extends Person{
 
+
+    private String Phone;
+
+    public buizz_contact(String name, String email, String phone) {
+        super(name, email);
+        Phone = phone;
+    }
+
+    public buizz_contact(String phone) {
+        Phone = phone;
+    }
+
+    public String getPhone() {
+        return Phone;
+    }
+
+    public void setPhone(String phone) {
+        Phone = phone;
+    }
+
+
+    @Override
+    public void display() {
+        System.out.println("the name is "+super.getName()+" and the email is "+ super.getEmail() + " and the number is "+Phone);
+
+    }
 }
+
